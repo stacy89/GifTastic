e2c6ff51785a4c92e4a394459e0f3cd9884f195c
Stacy Areas <stacyareas@gmail.com>
2018-01-29 20:46:28 -0800

    add function to display gifs -- add function to stop and start gif

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@ var foods = ["taco", "pizza", "cheese burger", "nachos"];
 
 function displayGifs() {
   var foodType = $(this).attr("data-name");
-  // console.log(foodType);
+  
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + foodType + "&api_key=a4CZ1y7uj55cHWUQgQFIwqiKoiRbZhib&limit=10";
 
   $.ajax({
@@ -10,38 +10,74 @@ function displayGifs() {
     method: "GET"
   })
   .done(function(response) {
-    console.log(response);
+    console.log(response.data);
+
+    var results = response.data;
+
+    for (i = 0; i < results.length; i++) {
+      var foodGifs = $("<img>");
+
+      foodGifs.addClass("gif");
+
+      foodGifs.attr("data-still", results[i].images.fixed_height_still.url);
+
+      foodGifs.attr("data-moving", results[i].images.fixed_height.url);
+
+      foodGifs.attr("data-state", "still");
+
+      foodGifs.attr("src", results[i].images.fixed_height_still.url);
+
+      $("#foodGifs").append(foodGifs);  
+    }
+    
+    $(".gif").on("click", function() {
+      $(".gif").empty();
+      var state = $(this).attr("data-state");
+
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-moving"));
+        $(this).attr("data-state", "animate")
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+    });
   });
 };
 
-// function generateButtons() {
-//   $("foodGifs").empty();
+function generateButtons() {
+  $("#gifButtonList").empty();
 
-//   for (i = 0; i < foods.length; i++) {
-//     var btn = $("<button>");
+  for (i = 0; i < foods.length; i++) {
+    var btn = $("<button>");
 
-//     btn.addClass("food");
+    btn.addClass("food");
 
-//     btn.attr("data-name", foods[i]);
+    btn.attr("data-name", foods[i]);
 
-//     btn.text(foods[i]);
+    btn.text(foods[i]);
 
-//     $("#gifButtonList").append(btn);
-//   }
-// };
+    $("#gifButtonList").append(btn);
+  }
+};
 
 
-// $("#search").on("click", function(event) {
-//   event.preventDefault();
+$("#search").on("click", function(event) {
+  event.preventDefault();
 
-//   var foodType = $("#foodType").val();
-//   foods.push(foodType);
+  var foodType = $("#foodType").val();
+  foods.push(foodType);
 
-//   generateButtons();
-// });  
+  $("#foodType").val("");
 
-// $(document).on("click", ".food", function() {
+  generateButtons();
+});  
 
-// });
+$(".food").on("click", function() {
+  var state = $(this).attr("data-state");
+}); 
+
+$(document).on("click", ".food", displayGifs);
+  generateButtons();
 
 
